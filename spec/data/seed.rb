09cae3674e97adcf2ead93d6798c3ab1706e8c01
przbadu <pushparaj.badu@gmail.com>
2017-06-16 15:50:42 +0545

    Add test cases for 2075 B.S to make sure it is working

--- spec/data/seed.rb
+++ spec/data/seed.rb
@@ -1,28 +1,237 @@
 module Seed
   DATA = [
-      {
-        nepali: [2073, 1, 1, 'Wednesday', 'Baishakh', 4],
-        english: [2016, 4, 13, 'Wednesday', 'April', 4]
-      },
-      {
-        nepali: [2073, 1, 2, 'Thursday', 'Baishakh', 5],
-        english: [2016, 4, 14, 'Thursday', 'April', 5]
-      },
-      {
-        nepali: [2073, 1, 15, 'Wednesday', 'Baishakh', 4],
-        english: [2016, 4, 27, 'Wednesday', 'April', 4]
-      },
-      {
-        nepali: [2073, 1, 30, 'Thursday', 'Baishakh', 5],
-        english: [2016, 5, 12, 'Thursday', 'May', 5]
-      },
-      {
-        nepali: [2073, 1, 31, 'Friday', 'Baishakh', 6],
-        english: [2016, 5, 13, 'Friday', 'May', 6]
-      },
-      {
-        nepali: [2073, 9, 22, 'Friday', 'Poush', 6],
-        english: [2017, 1, 6, 'Friday', 'January', 6]
-      }
+    {
+      nepali: [2073, 1, 1, 'Wednesday', 'Baishakh', 4],
+      english: [2016, 4, 13, 'Wednesday', 'April', 4]
+    },
+    {
+      nepali: [2073, 1, 2, 'Thursday', 'Baishakh', 5],
+      english: [2016, 4, 14, 'Thursday', 'April', 5]
+    },
+    {
+      nepali: [2073, 1, 15, 'Wednesday', 'Baishakh', 4],
+      english: [2016, 4, 27, 'Wednesday', 'April', 4]
+    },
+    {
+      nepali: [2073, 1, 30, 'Thursday', 'Baishakh', 5],
+      english: [2016, 5, 12, 'Thursday', 'May', 5]
+    },
+    {
+      nepali: [2073, 1, 31, 'Friday', 'Baishakh', 6],
+      english: [2016, 5, 13, 'Friday', 'May', 6]
+    },
+    {
+      nepali: [2073, 9, 22, 'Friday', 'Poush', 6],
+      english: [2017, 1, 6, 'Friday', 'January', 6]
+    },
+
+    {
+      nepali: [2074, 1, 1, 'Friday', 'Baishakh', 6],
+      english: [2017, 4, 14, 'Friday', 'April', 6]
+    },
+    {
+      nepali: [2074, 1, 15, 'Friday', 'Baishakh', 6],
+      english: [2017, 4, 28, 'Friday', 'April', 6]
+    },
+    {
+      nepali: [2074, 1, 17, 'Sunday', 'Baishakh', 1],
+      english: [2017, 4, 30, 'Sunday', 'April', 1]
+    },
+    {
+      nepali: [2074, 1, 18, 'Monday', 'Baishakh', 2],
+      english: [2017, 5, 1, 'Monday', 'May', 2]
+    },
+    {
+      nepali: [2074, 1, 31, 'Sunday', 'Baishakh', 1],
+      english: [2017, 5, 14, 'Sunday', 'May', 1]
+    },
+    {
+      nepali: [2074, 2, 1, 'Monday', 'Jestha', 2],
+      english: [2017, 5, 15, 'Monday', 'May', 2]
+    },
+    {
+      nepali: [2074, 2, 15, 'Monday', 'Jestha', 2],
+      english: [2017, 5, 29, 'Monday', 'May', 2]
+    },
+    {
+      nepali: [2074, 2, 17, 'Wednesday', 'Jestha', 4],
+      english: [2017, 5, 31, 'Wednesday', 'May', 4]
+    },
+    {
+      nepali: [2074, 2, 18, 'Thursday', 'Jestha', 5],
+      english: [2017, 6, 1, 'Thursday', 'June', 5]
+    },
+    {
+      nepali: [2074, 2, 31, 'Wednesday', 'Jestha', 4],
+      english: [2017, 6, 14, 'Wednesday', 'June', 4]
+    },
+    {
+      nepali: [2074, 3, 1, 'Thursday', 'Ashad', 5],
+      english: [2017, 6, 15, 'Thursday', 'June', 5]
+    },
+    {
+      nepali: [2074, 3, 15, 'Thursday', 'Ashad', 5],
+      english: [2017, 6, 29, 'Thursday', 'June', 5]
+    },
+    {
+      nepali: [2074, 3, 16, 'Friday', 'Ashad', 6],
+      english: [2017, 6, 30, 'Friday', 'June', 6]
+    },
+    {
+      nepali: [2074, 3, 17, 'Saturday', 'Ashad', 7],
+      english: [2017, 7, 1, 'Saturday', 'July', 7]
+    },
+    {
+      nepali: [2074, 3, 31, 'Saturday', 'Ashad', 7],
+      english: [2017, 7, 15, 'Saturday', 'July', 7]
+    },
+    {
+      nepali: [2074, 4, 1, 'Sunday', 'Shrawan', 1],
+      english: [2017, 7, 16, 'Sunday', 'July', 1]
+    },
+    {
+      nepali: [2074, 4, 16, 'Monday', 'Shrawan', 2],
+      english: [2017, 7, 31, 'Monday', 'July', 2]
+    },
+    {
+      nepali: [2074, 4, 17, 'Tuesday', 'Shrawan', 3],
+      english: [2017, 8, 1, 'Tuesday', 'August', 3]
+    },
+    {
+      nepali: [2074, 4, 32, 'Wednesday', 'Shrawan', 4],
+      english: [2017, 8, 16, 'Wednesday', 'August', 4]
+    },
+    {
+      nepali: [2074, 5, 1, 'Thursday', 'Bhadra', 5],
+      english: [2017, 8, 17, 'Thursday', 'August', 5]
+    },
+    {
+      nepali: [2074, 5, 15, 'Thursday', 'Bhadra', 5],
+      english: [2017, 8, 31, 'Thursday', 'August', 5]
+    },
+    {
+      nepali: [2074, 5, 16, 'Friday', 'Bhadra', 6],
+      english: [2017, 9, 1, 'Friday', 'September', 6]
+    },
+    {
+      nepali: [2074, 5, 31, 'Saturday', 'Bhadra', 7],
+      english: [2017, 9, 16, 'Saturday', 'September', 7]
+    },
+    {
+      nepali: [2074, 5, 31, 'Saturday', 'Bhadra', 7],
+      english: [2017, 9, 16, 'Saturday', 'September', 7]
+    },
+    {
+      nepali: [2074, 6, 1, 'Sunday', 'Ashwin', 1],
+      english: [2017, 9, 17, 'Sunday', 'September', 1]
+    },
+    {
+      nepali: [2074, 6, 14, 'Saturday', 'Ashwin', 7],
+      english: [2017, 9, 30, 'Saturday', 'September', 7]
+    },
+    {
+      nepali: [2074, 6, 15, 'Sunday', 'Ashwin', 1],
+      english: [2017, 10, 1, 'Sunday', 'October', 1]
+    },
+    {
+      nepali: [2074, 6, 31, 'Tuesday', 'Ashwin', 3],
+      english: [2017, 10, 17, 'Tuesday', 'October', 3]
+    },
+    {
+      nepali: [2074, 7, 1, 'Wednesday', 'Kartik', 4],
+      english: [2017, 10, 18, 'Wednesday', 'October', 4]
+    },
+    {
+      nepali: [2074, 7, 14, 'Tuesday', 'Kartik', 3],
+      english: [2017, 10, 31, 'Tuesday', 'October', 3]
+    },
+    {
+      nepali: [2074, 7, 15, 'Wednesday', 'Kartik', 4],
+      english: [2017, 11, 1, 'Wednesday', 'November', 4]
+    },
+    {
+      nepali: [2074, 7, 30, 'Thursday', 'Kartik', 5],
+      english: [2017, 11, 16, 'Thursday', 'November', 5]
+    },
+    {
+      nepali: [2074, 8, 1, 'Friday', 'Mangshir', 6],
+      english: [2017, 11, 17, 'Friday', 'November', 6]
+    },
+    {
+      nepali: [2074, 8, 14, 'Thursday', 'Mangshir', 5],
+      english: [2017, 11, 30, 'Thursday', 'November', 5]
+    },
+    {
+      nepali: [2074, 8, 15, 'Friday', 'Mangshir', 6],
+      english: [2017, 12, 1, 'Friday', 'December', 6]
+    },
+    {
+      nepali: [2074, 8, 29, 'Friday', 'Mangshir', 6],
+      english: [2017, 12, 15, 'Friday', 'December', 6]
+    },
+    {
+      nepali: [2074, 9, 1, 'Saturday', 'Poush', 7],
+      english: [2017, 12, 16, 'Saturday', 'December', 7]
+    },
+    {
+      nepali: [2074, 9, 16, 'Sunday', 'Poush', 1],
+      english: [2017, 12, 31, 'Sunday', 'December', 1]
+    },
+    {
+      nepali: [2074, 9, 17, 'Monday', 'Poush', 2],
+      english: [2018, 1, 1, 'Monday', 'January', 2]
+    },
+    {
+      nepali: [2074, 9, 30, 'Sunday', 'Poush', 1],
+      english: [2018, 1, 14, 'Sunday', 'January', 1]
+    },
+    {
+      nepali: [2074, 10, 1, 'Monday', 'Magh', 2],
+      english: [2018, 1, 15, 'Monday', 'January', 2]
+    },
+    {
+      nepali: [2074, 10, 17, 'Wednesday', 'Magh', 4],
+      english: [2018, 1, 31, 'Wednesday', 'January', 4]
+    },
+    {
+      nepali: [2074, 10, 18, 'Thursday', 'Magh', 5],
+      english: [2018, 2, 1, 'Thursday', 'February', 5]
+    },
+    {
+      nepali: [2074, 10, 29, 'Monday', 'Magh', 2],
+      english: [2018, 2, 12, 'Monday', 'February', 2]
+    },
+    {
+      nepali: [2074, 11, 1, 'Tuesday', 'Falgun', 3],
+      english: [2018, 2, 13, 'Tuesday', 'February', 3]
+    },
+    {
+      nepali: [2074, 11, 16, 'Wednesday', 'Falgun', 4],
+      english: [2018, 2, 28, 'Wednesday', 'February', 4]
+    },
+    {
+      nepali: [2074, 11, 17, 'Thursday', 'Falgun', 5],
+      english: [2018, 3, 1, 'Thursday', 'March', 5]
+    },
+    {
+      nepali: [2074, 11, 30, 'Wednesday', 'Falgun', 4],
+      english: [2018, 3, 14, 'Wednesday', 'March', 4]
+    },
+    {
+      nepali: [2074, 12, 1, 'Thursday', 'Chaitra', 5],
+      english: [2018, 3, 15, 'Thursday', 'March', 5]
+    },
+    {
+      nepali: [2074, 12, 17, 'Saturday', 'Chaitra', 7],
+      english: [2018, 3, 31, 'Saturday', 'March', 7]
+    },
+    {
+      nepali: [2074, 12, 18, 'Sunday', 'Chaitra', 1],
+      english: [2018, 4, 1, 'Sunday', 'April', 1]
+    },
+    {
+      nepali: [2074, 12, 30, 'Friday', 'Chaitra', 6],
+      english: [2018, 4, 13, 'Friday', 'April', 6]
+    },
   ]
 end
